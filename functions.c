7e386012d72fe786d30c7543479a0240e0a22a5f
MercierCorentin <corentin.mercier.pro@gmail.com>
2019-05-31 10:54:36 +0200

    Add "send_reply" function

--- functions.c
+++ functions.c
@@ -65,6 +65,84 @@ void get_formatted_time(char *string_date){
     sprintf(string_date, "Date: %s, %d %s %d %d:%d:%d GMT\r\n", days[tm.tm_wday], tm.tm_mday, months[tm.tm_mon], tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec);
 }
 
+int send_reply(int status, int client_socket, char request_path[BUFFER_SIZE + 16]){
+    char headers[1021] = { '\0' }, mime_type[15], string_date[38], string_file_size[11], *file_content = NULL, path[BUFFER_SIZE];
+    long int file_size = 0;
+    FILE *file_descriptor;
+
+    strcpy(path, request_path);
+
+    // Set status header and open right file depending on the status code
+    switch (status){
+    case 200:
+        strcat(headers, "HTTP/1.1 200 OK\r\n");
+        file_descriptor = fopen(request_path, "rb");
+        if(file_descriptor == NULL ){
+            send_reply(404, client_socket, "");
+            return 0;
+        }
+        break;
+    case 403:
+        strcat(headers, "HTTP/1.1 403 Forbidden\r\n");
+        strncpy(path, "/home/oem/c_projects/tcpip/resources/403.html", BUFFER_SIZE);
+        file_descriptor = fopen(path, "r");
+        break;
+    case 404:
+        strcat(headers, "HTTP/1.1 404 Not Found\r\n");
+        strncpy(path, "/home/oem/c_projects/tcpip/resources/404.html", BUFFER_SIZE);
+        fprintf(stderr, "%s\n", path);
+
+        file_descriptor = fopen(path, "r");
+        break;
+    case 405:
+        strcat(headers, "HTTP/1.1 405 Method Not Allowed\r\n");
+        strcpy(path, "/home/oem/c_projects/tcpip/resources/405.html");
+        file_descriptor = fopen(path, "r");
+        break;
+    default:
+        break;
+    }
+    
+    // Get the size of the file
+    fseek(file_descriptor, 0L, SEEK_END);
+    file_size = ftell(file_descriptor);
+    fseek(file_descriptor, 0L, SEEK_SET);
+
+    // Dynamic memory allocation then transfering the content of the file in a variable
+    file_content = (char *) malloc((file_size + 1) * sizeof(char));
+    fread(file_content, file_size, 1, file_descriptor);
+
+    // Int to string conversion (For Content-Length header)
+    sprintf(string_file_size, "%ld", file_size);
+    // Get mime type of file
+    get_mime_type(path, mime_type);
+    //  Get the actual time in the HTTP/1.1 RFC's format
+    get_formatted_time(string_date);
+
+    // adding headers
+    strcat(headers, string_date);
+    strcat(headers, "Content-Type: ");
+    strcat(headers, mime_type);
+    strcat(headers, "; charset=utf-8\r\n");
+    strcat(headers, "Content-Length: ");
+    strcat(headers, string_file_size);
+    strcat(headers, "\r\n\r\n");
+    // Send headers
+    write(client_socket, headers, strlen(headers));
+
+    // Send file content
+    if(send(client_socket, file_content, file_size + 1, 0) < file_size){
+        fprintf(stdout, RED"Help\n"RESET);
+    }else{
+        fprintf(stdout, GREEN"OK\n"RESET);
+    }
+
+    // Free memory, close file and exit function
+    free(file_content);
+    fclose(file_descriptor);
+    return 0;
+}
+
 int handle_request(int client_socket){
     // Initialisation
     char request[BUFFER_SIZE] = { '\0' }, character = { '\0' }, request_path[BUFFER_SIZE + 16] = WEB_ROOT, *file_content = NULL;
